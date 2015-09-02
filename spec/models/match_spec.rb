7b272d5090b57f419682c1df644d162b5551d570
Jake Quain <quainjn@gmail.com>
2015-09-01 21:48:27 -0400

    Fix failing time parse

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Match do
   describe "setting default date" do
     subject { Match.create }
-    let(:occured_at) { Time.parse("2011-03-27") }
+    let(:occured_at) { DateTime.parse("2011-03-27") }
     before { Time.stub(:now).and_return(occured_at) }
     its(:occured_at) { should == occured_at }
   end
