2d5d4ddbafc862c55ae36ade8ce6981e3eeda465
Jake Quain <quainjn@gmail.com>
2015-09-01 21:51:58 -0400

    Fix rspec deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
 
-  config.color_enabled = true
+  config.color = true
 end
