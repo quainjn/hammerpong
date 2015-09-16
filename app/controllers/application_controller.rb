7fac94f4e265c44f0431f52c444917c99695bfe0
Jake Quain <quainjn@gmail.com>
2015-09-16 15:58:11 -0400

    Redirect to new url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :check_for_old_domain
+
+  private
+
+  def check_for_old_domain
+    old_domain = ENV["OLD_DOMAIN"]
+    new_url = ENV["NEW_URL"]
+    if old_domain.present? && new_url.present? && request.url.to_s.include?(old_domain)
+      redirect_to new_url, status: :moved_permanently
+    end
+  end
 end
